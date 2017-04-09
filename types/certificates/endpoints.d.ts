7ab8129a7bb0fc0843229af3bd42ba492f5e9d6a
Rob Moran <robmor01@arm.com>
2017-04-09 22:45:05 +0100

    Dropped generated folders

--- types/certificates/endpoints.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConnectionOptions } from "../common/interfaces";
-import { AccountAdminApi } from "../_api/iam";
-import { DeveloperCertificateApi as DeveloperApi, ServerCredentialsApi as ServerApi } from "../_api/connector_ca";
-export declare class Endpoints {
-    developer: DeveloperApi;
-    server: ServerApi;
-    admin: AccountAdminApi;
-    constructor(options: ConnectionOptions);
-}
--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { AccessApi } from "./access";
-export { CertificatesApi } from "./certificates";
-export { DevicesApi } from "./devices";
-export { LoggingApi } from "./logging";
-export { UpdateApi } from "./update";
