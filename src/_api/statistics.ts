75ea04088d8972062c8e9a164e70cdeb1664f998
mbed Cloud (Circle CI) <support@mbed.com>
2017-05-08 13:48:21 +0000

    Add generated changes (2017-05-08 13:48:21.896066)

--- src/_api/statistics.ts
+++ src/_api/statistics.ts
@@ -77,7 +77,7 @@ export interface Metric {
      */
     "timestamp"?: string;
     /**
-     * Number of registered devices linked to the account.
+     * Maximum number of registered devices linked to the account.
      */
     "registered_devices"?: number;
     /**
@@ -115,11 +115,11 @@ export class AccountApi extends ApiBase {
      * Provides account-specific statistics for other cloud services.
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics. Supported values are:  - &#x60;transactions&#x60; - &#x60;registered_devices&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;handshakes_failed&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; 
-     * @param interval Group data by this interval in days, weeks or hours. Sample values: 2h, 3w, 4d. Maximum allowed interval is 9999[h/w/d]. 
+     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
      * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
      * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207. The parameter is not mandatory, if the period is specified. 
      * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207.The parameter is not mandatory, if the period is specified. 
-     * @param period Period. Fetch the data for the period in days, weeks or hours. Sample values: 2h, 3w, 4d. The parameter is not mandatory, if the start and end time are specified. Maximum allowed period is 9999[h/w/d]. 
+     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
      */
     v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
@@ -188,11 +188,11 @@ export class StatisticsApi extends ApiBase {
      * Provides account-specific statistics for other cloud services.
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics. Supported values are:  - &#x60;transactions&#x60; - &#x60;registered_devices&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;handshakes_failed&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; 
-     * @param interval Group data by this interval in days, weeks or hours. Sample values: 2h, 3w, 4d. Maximum allowed interval is 9999[h/w/d]. 
+     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
      * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
      * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207. The parameter is not mandatory, if the period is specified. 
      * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207.The parameter is not mandatory, if the period is specified. 
-     * @param period Period. Fetch the data for the period in days, weeks or hours. Sample values: 2h, 3w, 4d. The parameter is not mandatory, if the start and end time are specified. Maximum allowed period is 9999[h/w/d]. 
+     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
      */
     v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
