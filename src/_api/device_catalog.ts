08ac7bb13a238d5ca7f12f332f786e6ba6873cbb
mbed Cloud (Circle CI) <support@mbed.com>
2017-05-23 14:10:05 +0000

    Add generated changes (2017-05-23 14:10:05.654675)

--- src/_api/device_catalog.ts
+++ src/_api/device_catalog.ts
@@ -452,82 +452,10 @@ export class DefaultApi extends ApiBase {
         }, callback);
     }
     /** 
-     * List all device events (deprecated, use /v3/device-events/ instead).
-     * @param limit How many objects to retrieve in the page.
-     * @param order ASC or DESC
-     * @param after The ID of the the item after which to retrieve the next page.
-     * @param filter URL encoded query string parameter to filter returned data.  ##### Filtering &#x60;&#x60;&#x60;?filter&#x3D;{URL encoded query string}&#x60;&#x60;&#x60;  The query string is made up of key/value pairs separated by ampersands. So for a query of &#x60;&#x60;&#x60;key1&#x3D;value1&amp;key2&#x3D;value2&amp;key3&#x3D;value3&#x60;&#x60;&#x60; this would be encoded as follows: &#x60;&#x60;&#x60;?filter&#x3D;key1%3Dvalue1%26key2%3Dvalue2%26key3%3Dvalue3&#x60;&#x60;&#x60; The examples below show the queries in *unencoded* form.  ###### By id: &#x60;&#x60;&#x60;id&#x3D;{id}&#x60;&#x60;&#x60;  ###### By state change: &#x60;&#x60;&#x60;state_change&#x3D;[True|False]&#x60;&#x60;&#x60;  ###### By event type: &#x60;&#x60;&#x60;event_type&#x3D;{value}&#x60;&#x60;&#x60;  ###### On date-time fields: Date-time fields should be specified in UTC RFC3339 format &#x60;&#x60;&#x60;YYYY-MM-DDThh:mm:ss.msZ&#x60;&#x60;&#x60;. There are three permitted variations:  * UTC RFC3339 with milliseconds e.g. 2016-11-30T16:25:12.1234Z * UTC RFC3339 without milliseconds e.g. 2016-11-30T16:25:12Z * UTC RFC3339 shortened - without milliseconds and punctuation e.g. 20161130T162512Z  Date-time filtering supports three operators:  * equality * greater than or equal to &amp;ndash; field name suffixed with &#x60;&#x60;&#x60;__gte&#x60;&#x60;&#x60; * less than or equal to &amp;ndash; field name suffixed with &#x60;&#x60;&#x60;__lte&#x60;&#x60;&#x60;  Lower and upper limits to a date-time range may be specified by including both the &#x60;&#x60;&#x60;__gte&#x60;&#x60;&#x60; and &#x60;&#x60;&#x60;__lte&#x60;&#x60;&#x60; forms in the filter.  &#x60;&#x60;&#x60;{field name}[|__lte|__gte]&#x3D;{UTC RFC3339 date-time}&#x60;&#x60;&#x60;  ##### Multi-field example  &#x60;&#x60;&#x60;id&#x3D;0158d38771f70000000000010010038c&amp;state_change&#x3D;True&amp;date_time__gte&#x3D;2016-11-30T16:25:12.1234Z&#x60;&#x60;&#x60;  Encoded:  &#x60;&#x60;&#x60;?filter&#x3D;id%3D0158d38771f70000000000010010038c%26state_change%3DTrue%26date_time__gte%3D2016-11-30T16%3A25%3A12.1234Z&#x60;&#x60;&#x60;
-     * @param include Comma separated list of data fields to return. Currently supported: total_count
-     */
-    deviceEventList_1 (limit?: number, order?: string, after?: string, filter?: string, include?: string, callback?: (error:any, data?:DeviceEventPage, response?: superagent.Response) => any): superagent.SuperAgentRequest {
-
-        let headerParams: any = {};
-
-        let queryParameters: any = {};
-        if (limit !== undefined) {
-            queryParameters['limit'] = limit;
-        }
-        if (order !== undefined) {
-            queryParameters['order'] = order;
-        }
-        if (after !== undefined) {
-            queryParameters['after'] = after;
-        }
-        if (filter !== undefined) {
-            queryParameters['filter'] = filter;
-        }
-        if (include !== undefined) {
-            queryParameters['include'] = include;
-        }
-
-        let useFormData = false;
-        let formParams: any = {};
-
-        return this.request({
-            url: '/v3/devicelog/',
-            method: 'GET',
-            headers: headerParams,
-            query: queryParameters,
-            useFormData: useFormData,
-            formParams: formParams,
-            json: true,
-        }, callback);
-    }
-    /** 
-     * Retrieve device event (deprecated, use /v3/device-events/{device_event_id}/ instead)
-     * @param deviceEventId 
-     */
-    deviceEventRetrieve (deviceEventId: string, callback?: (error:any, data?:DeviceEventData, response?: superagent.Response) => any): superagent.SuperAgentRequest {
-        // verify required parameter "deviceEventId" is set
-        if (deviceEventId === null || deviceEventId === undefined) {
-            if (callback) {
-                callback(new SDKError("Required parameter 'deviceEventId' missing."));
-            }
-            return;
-        }
-
-        let headerParams: any = {};
-
-        let queryParameters: any = {};
-
-        let useFormData = false;
-        let formParams: any = {};
-
-        return this.request({
-            url: '/v3/devicelog/{device_event_id}/'.replace('{' + 'device_event_id' + '}', String(deviceEventId)),
-            method: 'GET',
-            headers: headerParams,
-            query: queryParameters,
-            useFormData: useFormData,
-            formParams: formParams,
-            json: true,
-        }, callback);
-    }
-    /** 
      * Retrieve device event
      * @param deviceEventId 
      */
-    deviceEventRetrieve_2 (deviceEventId: string, callback?: (error:any, data?:DeviceEventData, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+    deviceEventRetrieve (deviceEventId: string, callback?: (error:any, data?:DeviceEventData, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "deviceEventId" is set
         if (deviceEventId === null || deviceEventId === undefined) {
             if (callback) {
@@ -587,6 +515,78 @@ export class DefaultApi extends ApiBase {
 
         return this.request({
             url: '/v3/devices/',
+            method: 'GET',
+            headers: headerParams,
+            query: queryParameters,
+            useFormData: useFormData,
+            formParams: formParams,
+            json: true,
+        }, callback);
+    }
+    /** 
+     * List all device events (deprecated, use /v3/device-events/ instead).
+     * @param limit How many objects to retrieve in the page.
+     * @param order ASC or DESC
+     * @param after The ID of the the item after which to retrieve the next page.
+     * @param filter URL encoded query string parameter to filter returned data.  ##### Filtering &#x60;&#x60;&#x60;?filter&#x3D;{URL encoded query string}&#x60;&#x60;&#x60;  The query string is made up of key/value pairs separated by ampersands. So for a query of &#x60;&#x60;&#x60;key1&#x3D;value1&amp;key2&#x3D;value2&amp;key3&#x3D;value3&#x60;&#x60;&#x60; this would be encoded as follows: &#x60;&#x60;&#x60;?filter&#x3D;key1%3Dvalue1%26key2%3Dvalue2%26key3%3Dvalue3&#x60;&#x60;&#x60; The examples below show the queries in *unencoded* form.  ###### By id: &#x60;&#x60;&#x60;id&#x3D;{id}&#x60;&#x60;&#x60;  ###### By state change: &#x60;&#x60;&#x60;state_change&#x3D;[True|False]&#x60;&#x60;&#x60;  ###### By event type: &#x60;&#x60;&#x60;event_type&#x3D;{value}&#x60;&#x60;&#x60;  ###### On date-time fields: Date-time fields should be specified in UTC RFC3339 format &#x60;&#x60;&#x60;YYYY-MM-DDThh:mm:ss.msZ&#x60;&#x60;&#x60;. There are three permitted variations:  * UTC RFC3339 with milliseconds e.g. 2016-11-30T16:25:12.1234Z * UTC RFC3339 without milliseconds e.g. 2016-11-30T16:25:12Z * UTC RFC3339 shortened - without milliseconds and punctuation e.g. 20161130T162512Z  Date-time filtering supports three operators:  * equality * greater than or equal to &amp;ndash; field name suffixed with &#x60;&#x60;&#x60;__gte&#x60;&#x60;&#x60; * less than or equal to &amp;ndash; field name suffixed with &#x60;&#x60;&#x60;__lte&#x60;&#x60;&#x60;  Lower and upper limits to a date-time range may be specified by including both the &#x60;&#x60;&#x60;__gte&#x60;&#x60;&#x60; and &#x60;&#x60;&#x60;__lte&#x60;&#x60;&#x60; forms in the filter.  &#x60;&#x60;&#x60;{field name}[|__lte|__gte]&#x3D;{UTC RFC3339 date-time}&#x60;&#x60;&#x60;  ##### Multi-field example  &#x60;&#x60;&#x60;id&#x3D;0158d38771f70000000000010010038c&amp;state_change&#x3D;True&amp;date_time__gte&#x3D;2016-11-30T16:25:12.1234Z&#x60;&#x60;&#x60;  Encoded:  &#x60;&#x60;&#x60;?filter&#x3D;id%3D0158d38771f70000000000010010038c%26state_change%3DTrue%26date_time__gte%3D2016-11-30T16%3A25%3A12.1234Z&#x60;&#x60;&#x60;
+     * @param include Comma separated list of data fields to return. Currently supported: total_count
+     */
+    deviceLogList (limit?: number, order?: string, after?: string, filter?: string, include?: string, callback?: (error:any, data?:DeviceEventPage, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+
+        let headerParams: any = {};
+
+        let queryParameters: any = {};
+        if (limit !== undefined) {
+            queryParameters['limit'] = limit;
+        }
+        if (order !== undefined) {
+            queryParameters['order'] = order;
+        }
+        if (after !== undefined) {
+            queryParameters['after'] = after;
+        }
+        if (filter !== undefined) {
+            queryParameters['filter'] = filter;
+        }
+        if (include !== undefined) {
+            queryParameters['include'] = include;
+        }
+
+        let useFormData = false;
+        let formParams: any = {};
+
+        return this.request({
+            url: '/v3/devicelog/',
+            method: 'GET',
+            headers: headerParams,
+            query: queryParameters,
+            useFormData: useFormData,
+            formParams: formParams,
+            json: true,
+        }, callback);
+    }
+    /** 
+     * Retrieve device event (deprecated, use /v3/device-events/{device_event_id}/ instead)
+     * @param deviceEventId 
+     */
+    deviceLogRetrieve (deviceEventId: string, callback?: (error:any, data?:DeviceEventData, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+        // verify required parameter "deviceEventId" is set
+        if (deviceEventId === null || deviceEventId === undefined) {
+            if (callback) {
+                callback(new SDKError("Required parameter 'deviceEventId' missing."));
+            }
+            return;
+        }
+
+        let headerParams: any = {};
+
+        let queryParameters: any = {};
+
+        let useFormData = false;
+        let formParams: any = {};
+
+        return this.request({
+            url: '/v3/devicelog/{device_event_id}/'.replace('{' + 'device_event_id' + '}', String(deviceEventId)),
             method: 'GET',
             headers: headerParams,
             query: queryParameters,
