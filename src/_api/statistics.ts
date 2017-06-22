0764a531dd0461acd3390940f7101ea92d2b583c
mbed Cloud (Circle CI) <support@mbed.com>
2017-06-22 13:48:08 +0000

    Add generated changes (2017-06-22 13:48:08.964338)

--- src/_api/statistics.ts
+++ src/_api/statistics.ts
@@ -103,6 +103,26 @@ export interface Metric {
 }
 
 export interface SuccessfulResponse {
+    /**
+     * continuation_token included in the request or null.
+     */
+    "after"?: string;
+    /**
+     * true when there are more results to fetch using the included continuation_token.
+     */
+    "has_more"?: boolean;
+    /**
+     * API resource name.
+     */
+    "object"?: string;
+    /**
+     * limit used in the request to retrieve the results.
+     */
+    "limit"?: number;
+    /**
+     * token to use in after request parameter to fetch more results.
+     */
+    "continuation_token"?: string;
     "data"?: Array<Metric>;
 }
 
@@ -115,13 +135,16 @@ export class AccountApi extends ApiBase {
      * Provides account-specific statistics for other cloud services.
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics. Supported values are:  - &#x60;transactions&#x60; - &#x60;registered_devices&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;handshakes_failed&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; 
-     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
+     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The maximum interval cannot exceed more than one year ( 365 days ) and so the allowed ranges are 5m - 525600m / 1h - 8760h / 1d - 365d / 1w - 53w. 
      * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
-     * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207. The parameter is not mandatory, if the period is specified. 
-     * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207.The parameter is not mandatory, if the period is specified. 
-     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
+     * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year (365 days). The parameter is not mandatory, if the period is specified. 
+     * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year ( 365 days ). The parameter is not mandatory, if the period is specified. 
+     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. The maximum period cannot exceed more than one year ( 365 days ) and so the allowed ranges are 5m - 525600m / 1h - 8760h / 1d - 365d / 1w - 53w. 
+     * @param limit Limit the number of results returned. Default value is 50, minimum value is 2 and maximum value is 1000. 
+     * @param after The continuous_token included in the previous response to retrieve the next page of results. 
+     * @param order The order to sort the results. Default value is ASC. Allowed values are ASC / DESC. 
      */
-    v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+    v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
         if (include === null || include === undefined) {
             if (callback) {
@@ -162,6 +185,15 @@ export class AccountApi extends ApiBase {
         }
         if (interval !== undefined) {
             queryParameters['interval'] = interval;
+        }
+        if (limit !== undefined) {
+            queryParameters['limit'] = limit;
+        }
+        if (after !== undefined) {
+            queryParameters['after'] = after;
+        }
+        if (order !== undefined) {
+            queryParameters['order'] = order;
         }
 
         let useFormData = false;
@@ -188,13 +220,16 @@ export class StatisticsApi extends ApiBase {
      * Provides account-specific statistics for other cloud services.
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics. Supported values are:  - &#x60;transactions&#x60; - &#x60;registered_devices&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;handshakes_failed&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; 
-     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
+     * @param interval Group data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The maximum interval cannot exceed more than one year ( 365 days ) and so the allowed ranges are 5m - 525600m / 1h - 8760h / 1d - 365d / 1w - 53w. 
      * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
-     * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207. The parameter is not mandatory, if the period is specified. 
-     * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z/2017-02-07T09:20:56.990Z/2017/20170207.The parameter is not mandatory, if the period is specified. 
-     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. Allowed range is 1[h/d/w] - 9999[h/d/w] or 5[m] - 9999[m]. 
+     * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year (365 days). The parameter is not mandatory, if the period is specified. 
+     * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year ( 365 days ). The parameter is not mandatory, if the period is specified. 
+     * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. The maximum period cannot exceed more than one year ( 365 days ) and so the allowed ranges are 5m - 525600m / 1h - 8760h / 1d - 365d / 1w - 53w. 
+     * @param limit Limit the number of results returned. Default value is 50, minimum value is 2 and maximum value is 1000. 
+     * @param after The continuous_token included in the previous response to retrieve the next page of results. 
+     * @param order The order to sort the results. Default value is ASC. Allowed values are ASC / DESC. 
      */
-    v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+    v3MetricsGet (include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error:any, data?:SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
         if (include === null || include === undefined) {
             if (callback) {
@@ -235,6 +270,15 @@ export class StatisticsApi extends ApiBase {
         }
         if (interval !== undefined) {
             queryParameters['interval'] = interval;
+        }
+        if (limit !== undefined) {
+            queryParameters['limit'] = limit;
+        }
+        if (after !== undefined) {
+            queryParameters['after'] = after;
+        }
+        if (order !== undefined) {
+            queryParameters['order'] = order;
         }
 
         let useFormData = false;
