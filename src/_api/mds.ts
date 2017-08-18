015e6ab753138ff0f71a2131c3575b9bcfeb4c62
mbed Cloud (Circle CI) <monty-bot@arm.com>
2017-08-18 11:48:16 +0000

    Add generated changes (2017-08-18 11:48:16.616046)

--- src/_api/mds.ts
+++ src/_api/mds.ts
@@ -150,7 +150,7 @@ export interface Resource {
     /**
      * The URL of the resource.
      */
-    "uri"?: string;
+    "uri": string;
     /**
      * Observable determines whether you can subscribe to changes for this resource. It can have values \"true\" or \"false\". 
      */
@@ -187,7 +187,7 @@ export interface Webhook {
     /**
      * The URL to which the notifications are sent. We recommend that you serve this URL over HTTPS.
      */
-    "url"?: string;
+    "url": string;
     /**
      * Headers (key/value) that are sent with the notification. Optional.
      */
@@ -542,7 +542,7 @@ export class ResourcesApi extends ApiBase {
      * With this API, you can write new values to existing resources, or create new  resources on the device. The resource-path does not have to exist - it can be  created by the call. The maximum length of resource-path is 255 characters.  This API can also be used to transfer files to the device. mbed Cloud Connect LWM2M server implements the Option 1 from RFC7959. The maximum block size is 1024 bytes. The block size versus transferred file size is something to note in low quality networks. The customer application needs to know what type of file is transferred (for example txt) and the payload can be encrypted by the customer. The maximum size of payload is 1048576 bytes.  All resource APIs are asynchronous. These APIs respond only if the device is turned on and connected to mbed Cloud Connect and there is an active notification channel.  **Example usage:**      curl -X PUT \\       https://api.us-east-1.mbedcloud.com/v2/endpoints/{device-id}/{resourcePath} \\       -H &#39;authorization: Bearer {api-key}&#39; -d &#39;{payload}&#39; 
      * @param deviceId A unique mbed Cloud device ID for the endpoint. Note that the ID must be an exact match. You cannot use wildcards here. 
      * @param resourcePath Resource URL.
-     * @param resourceValue The value to be set to the resource. (Check accceptable content-types) 
+     * @param resourceValue The value to be set to the resource. 
      * @param noResp **Non-confirmable requests**  All resource APIs have the parameter noResp. If you make a request with &#x60;noResp&#x3D;true&#x60;, mbed Cloud Connect makes a CoAP non-confirmable request to the device. Such requests are not guaranteed to arrive in the device, and you do not get back an async-response-id.  If calls with this parameter enabled succeed, they return with the status code 204 No Content. If the underlying protocol does not support non-confirmable requests, or if the endpoint is registered in queue mode, the response is status code 409 Conflict. 
      */
     v2EndpointsDeviceIdResourcePathPut (deviceId: string, resourcePath: string, resourceValue: string, noResp?: boolean, callback?: (error:any, data?:AsyncID, response?: superagent.Response) => any): superagent.SuperAgentRequest {
