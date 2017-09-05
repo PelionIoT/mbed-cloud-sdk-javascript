3caff468db3bc9ca5e3054d02a5fa26e88369150
mbed Cloud (Circle CI) <monty-bot@arm.com>
2017-09-05 12:17:43 +0000

    Add generated changes (2017-09-05 12:17:43.861404)

--- src/_api/statistics.ts
+++ src/_api/statistics.ts
@@ -150,7 +150,6 @@ export class AccountApi extends ApiBase {
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics and total_count (if included, the response will contain total_count to specify the total number of records available). Supported values are:  - &#x60;transactions&#x60; - &#x60;full_registrations&#x60; - &#x60;registration_updates&#x60; - &#x60;deleted_registrations&#x60; - &#x60;expired_registrations&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; - &#x60;total_count&#x60; 
      * @param interval Group the data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The maximum interval cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w. 
-     * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
      * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year (365 days). The parameter is not mandatory, if the period is specified. 
      * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year ( 365 days ). The parameter is not mandatory, if the period is specified. 
      * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. The maximum period cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w. 
@@ -158,7 +157,7 @@ export class AccountApi extends ApiBase {
      * @param after The metric ID after which to start fetching. 
      * @param order The order of the records to return. Available values are ASC and DESC. The default value is ASC. 
      */
-    public v3MetricsGet(include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error: any, data?: SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+    public v3MetricsGet(include: string, interval: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error: any, data?: SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
         if (include === null || include === undefined) {
             if (callback) {
@@ -173,16 +172,8 @@ export class AccountApi extends ApiBase {
             }
             return;
         }
-        // verify required parameter "authorization" is set
-        if (authorization === null || authorization === undefined) {
-            if (callback) {
-                callback(new SDKError("Required parameter 'authorization' missing."));
-            }
-            return;
-        }
 
         const headerParams: any = {};
-        headerParams["Authorization"] = authorization;
 
         const queryParameters: any = {};
         if (include !== undefined) {
@@ -234,7 +225,6 @@ export class StatisticsApi extends ApiBase {
      * This REST API is used to get account-specific statistics.
      * @param include A comma-separated list of requested metrics and total_count (if included, the response will contain total_count to specify the total number of records available). Supported values are:  - &#x60;transactions&#x60; - &#x60;full_registrations&#x60; - &#x60;registration_updates&#x60; - &#x60;deleted_registrations&#x60; - &#x60;expired_registrations&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; - &#x60;total_count&#x60; 
      * @param interval Group the data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The maximum interval cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w. 
-     * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics. 
      * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year (365 days). The parameter is not mandatory, if the period is specified. 
      * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year ( 365 days ). The parameter is not mandatory, if the period is specified. 
      * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. The maximum period cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w. 
@@ -242,7 +232,7 @@ export class StatisticsApi extends ApiBase {
      * @param after The metric ID after which to start fetching. 
      * @param order The order of the records to return. Available values are ASC and DESC. The default value is ASC. 
      */
-    public v3MetricsGet(include: string, interval: string, authorization: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error: any, data?: SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
+    public v3MetricsGet(include: string, interval: string, start?: string, end?: string, period?: string, limit?: number, after?: string, order?: string, callback?: (error: any, data?: SuccessfulResponse, response?: superagent.Response) => any): superagent.SuperAgentRequest {
         // verify required parameter "include" is set
         if (include === null || include === undefined) {
             if (callback) {
@@ -257,16 +247,8 @@ export class StatisticsApi extends ApiBase {
             }
             return;
         }
-        // verify required parameter "authorization" is set
-        if (authorization === null || authorization === undefined) {
-            if (callback) {
-                callback(new SDKError("Required parameter 'authorization' missing."));
-            }
-            return;
-        }
 
         const headerParams: any = {};
-        headerParams["Authorization"] = authorization;
 
         const queryParameters: any = {};
         if (include !== undefined) {
