f7186803e6d07838388939cfe854b1ffae23f3e0
Rob Moran <rob.moran@arm.com>
2017-09-25 19:17:04 +0100

    IOTTOO-2900 made device execution mode read only

--- src/deviceDirectory/models/deviceAdapter.ts
+++ src/deviceDirectory/models/deviceAdapter.ts
@@ -72,7 +72,6 @@ export class DeviceAdapter {
             bootstrapped_timestamp:       from.bootstrappedTimestamp,
             ca_id:                        from.certificateIssuerId,
             connector_expiration_date:    from.connectorCertificateExpiration,
-            device_execution_mode:        from.deviceExecutionMode,
             device_key:                   from.certificateFingerprint,
             endpoint_name:                from.alias,
             firmware_checksum:            from.firmwareChecksum,
--- src/deviceDirectory/types.ts
+++ src/deviceDirectory/types.ts
@@ -73,10 +73,6 @@ export interface AddDeviceObject extends DeviceObject {
      */
     deviceClass?: string;
     /**
-     * The certificate type
-     */
-    deviceExecutionMode?: number;
-    /**
      * The serial number of the device
      */
     serialNumber?: string;
