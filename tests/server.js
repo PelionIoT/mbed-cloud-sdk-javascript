2c6c0a89c17aa84d963b896f363aaf0fd337dfa9
Rob Moran <rob.moran@arm.com>
2017-10-18 11:16:52 +0100

    Key log

--- tests/server.js
+++ tests/server.js
@@ -22,6 +22,7 @@ if (!config.apiKey) {
 if (config.host) {
     console.log(`Using host ${config.host}`);
 }
+console.log(`Using key ${envVarKey}`);
 
 var modules = {
     AccountManagementApi: new MbedCloudSDK.AccountManagementApi(config),
