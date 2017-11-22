95abc46a97c23c935c9414dcd22de4599627f125
David Hyman <david.hyman@tesco.net>
2017-11-22 12:09:02 +0000

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -12,5 +12,13 @@ function finish {
 }
 trap finish EXIT
 
-sleep 5
-python -u testrunner/bin/trunner -s http://localhost:5000
+docker run --rm --net=host --name=testrunner_container\
+# where our SDK server is located
+-e "TEST_SERVER_URL=http://127.0.0.1:5000"\
+# host-relative path to fixtures mountpoint
+-e "TEST_FIXTURES_DIR=/home/ubuntu/cci_fixtures"\
+# configure the fixtures mountpoint
+-v /home/ubuntu/cci_fixtures:/runner/test_fixtures\
+# configure the results mountpoint
+-v ./integration_results:/runner/results\
+${INTEGRATION_DOCKER}
