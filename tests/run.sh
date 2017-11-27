721da040dafc86a729b0e646fb1a17fb9b4124f2
David Hyman <david.hyman@tesco.net>
2017-11-27 23:40:31 +0000

    docker_testrunner (#194)
    * Update circle.yml
    
    * Update run.sh
    
    * Update circle.yml
    
    * Update circle.yml
    
    * Update run.sh
    
    * Update circle.yml
    
    * Update circle.yml
    
    * Update run.sh
    
    * Update run.sh
    
    * Update circle.yml
    
    * Update run.sh
    
    * Update run.sh
    
    * trying with ${PROJECT_REPORTS}
    
    * "change the setting at the top to use the working directory"
    
    * npm coverage writes to a 'coverage' directory
    
    so lets see if that's where the results are
    
    * Update run.sh
    
    * keep original standardout scraper
    
    * re-add git creds
    
    * re-add sleep
    
    * indent
    
    * Add executeResource mapping
    
    * Update circle.yml

--- tests/run.sh
+++ tests/run.sh
@@ -12,5 +12,18 @@ function finish {
 }
 trap finish EXIT
 
+# run docker and remove container afterwards
+#   where our SDK server is located
+#   host-relative path to fixtures mountpoint
+#   configure the fixtures mountpoint
+#   configure the results mountpoint
+#   image name
+
+# sleep to allow coverage to get its hooks in
 sleep 5
-python -u testrunner/bin/trunner -s http://localhost:5000
+
+docker run --rm --net=host --name=testrunner_container \
+-e "TEST_SERVER_URL=http://127.0.0.1:5000" \
+-e "TEST_FIXTURES_DIR=/home/ubuntu/cci_fixtures" \
+-v /home/ubuntu/cci_fixtures:/runner/test_fixtures \
+${TESTRUNNER_DOCKER_IMAGE}
