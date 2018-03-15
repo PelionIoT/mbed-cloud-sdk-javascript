3b5ff61c6639f5da6ef1086e76a25ae19937a046
Alex Logan <Alex.Logan@arm.com>
2018-03-15 11:07:00 +0000

    add contractNumber to account

--- src/accountManagement/models/accountAdapter.ts
+++ src/accountManagement/models/accountAdapter.ts
@@ -69,7 +69,8 @@ export class AccountAdapter {
             notificationEmails              : from.notification_emails,
             referenceNote                   : from.reference_note,
             updatedAt                       : from.updated_at,
-            customProperties                : from.account_properties
+            customProperties: from.account_properties,
+            contractNumber                  : from.contract_number,
         }, api);
     }
 
