7df2123903f1efd96ae53601585e0a03476e50e8
Alex Logan <Alex.Logan@arm.com>
2018-10-18 16:53:00 +0100

    comment out unused vars

--- src/common/apiBase.ts
+++ src/common/apiBase.ts
@@ -23,12 +23,12 @@ import { SDKError } from "./sdkError";
 import { ConnectionOptions } from "./interfaces";
 
 // tslint:disable-next-line:no-var-requires
-const packageInformation = require("../../package.json");
+// const packageInformation = require("../../package.json");
 const DATE_REGEX = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
 const JSON_REGEX = /^application\/json(;.*)?$/i;
 const MIME_REGEX = /^text\/plain(;.*)?$/i;
-const VERSION = packageInformation.is_published ? packageInformation.version : `${packageInformation.version}+dev`;
-const userAgent = `${packageInformation.name}-javascript / ${VERSION}`;
+// const VERSION = packageInformation.is_published ? packageInformation.version : `${packageInformation.version}+dev`;
+// const userAgent = `${packageInformation.name}-javascript / ${VERSION}`;
 
 export class ApiBase {
 
