87105d2fd5e11280f60db97b73843af5edcae8ad
Alex Logan <Alex.Logan@arm.com>
2019-01-14 15:58:56 +0000

    ❌ remove foundation required fields

--- generator/generators/generateInterface.ts
+++ generator/generators/generateInterface.ts
@@ -13,9 +13,7 @@ export async function generateInterface(entity: any, enums: any): Promise<string
         const type = getPropertyType(f, enums);
         const name = snakeToCamel(f._key);
 
-        const readOnly = !!f.readOnly;
-        const required = !!f.required;
-        const property = new PropertyContainer(name, type, { isInterface: true, isOptional: !required, isReadonly: readOnly });
+        const property = new PropertyContainer(name, type, { isInterface: true, isOptional: true });
         properties.push(property);
 
         if (f.enum_reference) {
