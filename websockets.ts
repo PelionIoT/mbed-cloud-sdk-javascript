0e28f5ed891aee82fc2d63917d049e52d0897fce
Alex Logan <Alex.Logan@arm.com>
2019-02-05 15:19:04 +0000

    more sockets

--- websockets.ts
+++ websockets.ts
@@ -1,48 +1,63 @@
 // import { WebsocketApi } from "../../src/_api/mds";
-import { client as WebsocketClient } from "websocket";
+import { ConnectApi } from "./src";
+import { w3cwebsocket as WebsocketClient } from "websocket";
 
-const apiKey = "ak_1MDE2ODVjNzVjOTdjMjJjMTFjMDQ0ZDUzMDAwMDAwMDA01685c7a82ee22c11c044d5300000000VjQ6rseowvjwo18BHGiBv47c4X4xNPhS";
-// const host = "https://api-ns-websocket.mbedcloudintegration.net";
+const apiKey = "ak_1MDE2ODVjNzVjOTdjMjJjMTFjMDQ0ZDUzMDAwMDAwMDA0168b949978cf65f42b0f3e1000000000DNrDQsaXkJWwEQLcbbPUnWafTigsGbn";
+const host = "https://api-ns-websocket.mbedcloudintegration.net";
 
-const websocketClient = new WebsocketClient();
+const api = new ConnectApi({
+    apiKey,
+    host,
+});
 
-websocketClient.on("connectFailed", error => {
+api.listConnectedDevices().then(devices => {
+    const myDevice = devices.data[0];
+    api.subscribe.resourceValues({
+        deviceId: myDevice.id,
+        resourcePaths: [
+            "/3200/0/5501"
+        ]
+    });
+});
+
+const websocketClient = new WebsocketClient(
+    "wss://api-ns-websocket.mbedcloudintegration.net/v2/notification/websocket-connect",
+    [
+        `pelion_${apiKey}`,
+        `wss`
+    ],
+);
+
+websocketClient.onerror = error => {
     // tslint:disable-next-line:no-console
     console.log("Connect Error: " + error.toString());
-});
+};
 
-websocketClient.on("connect", connection => {
+websocketClient.onopen = () => {
     // tslint:disable-next-line:no-console
     console.log("WebSocket Client Connected");
+};
 
+websocketClient.onclose = () => {
     // tslint:disable-next-line:no-console
-    console.log(connection);
+    console.log("echo-protocol Connection Closed");
 
-    connection.on("error", error => {
+    const connectionInfo = (websocketClient as any)._connection;
+    if (connectionInfo) {
         // tslint:disable-next-line:no-console
-        console.log("Connection Error: " + error.toString());
-    });
-
-    connection.on("close", () => {
+        console.log(connectionInfo.closeReasonCode);
         // tslint:disable-next-line:no-console
-        console.log("echo-protocol Connection Closed");
-    });
+        console.log(connectionInfo.closeDescription);
+    }
+    process.exit();
+};
 
-    connection.on("message", message => {
-        if (message.type === "utf8") {
-            // tslint:disable-next-line:no-console
-            console.log("Received: '" + message.utf8Data + "'");
-        }
-    });
-});
-
-websocketClient.connect(
-    "https://api-ns-websocket.mbedcloudintegration.net/v2/notification/websocket-connect",
-    [],
-    null,
-    {
-        "Sec-WebSocket-Protocol": `pelion_${apiKey}, wss`
-    });
+websocketClient.onmessage = message => {
+    if (message.type === "utf8") {
+        // tslint:disable-next-line:no-console
+        console.log("Received: '" + message.utf8Data + "'");
+    }
+};
 
 process.stdin.resume();
 
