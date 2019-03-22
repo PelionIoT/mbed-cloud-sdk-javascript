b8d84db082f859bf5c38030adfbf94a03e84b91c
Alex Logan <Alex.Logan@arm.com>
2019-03-22 15:09:29 +0000

    multiple api key tests

--- src/common/config.ts
+++ src/common/config.ts
@@ -3,20 +3,20 @@ import dotenv = require("dotenv");
 import { SDKLogLevel } from "./logger";
 
 export class Config {
-    private readonly ENV_API_KEY = "MBED_CLOUD_SDK_API_KEY";
-    private readonly ENV_HOST = "MBED_CLOUD_SDK_HOST";
-    private readonly ENV_LOG_LEVEL = "MBED_CLOUD_SDK_LOG_LEVEL";
+    private static readonly ENV_API_KEY = "MBED_CLOUD_SDK_API_KEY";
+    private static readonly ENV_HOST = "MBED_CLOUD_SDK_HOST";
+    private static readonly ENV_LOG_LEVEL = "MBED_CLOUD_SDK_LOG_LEVEL";
 
-    public readonly apiKey: string;
-    public readonly host: string;
-    public readonly logLevel: SDKLogLevel;
+    public readonly apiKey?: string;
+    public readonly host?: string;
+    public readonly logLevel?: SDKLogLevel;
 
     constructor(options?: ConnectionOptions) {
         options = options || {};
         if (dotenv && typeof dotenv.config === "function") { dotenv.config(); }
-        this.apiKey = options.apiKey || (process && process.env[this.ENV_API_KEY]) || "default";
-        this.host = options.host || (process && process.env[this.ENV_HOST]) || "https://api.us-east-1.mbedcloud.com";
-        this.logLevel = options.logLevel || (process && (process.env[this.ENV_LOG_LEVEL]) as SDKLogLevel) || "WARN";
+        this.apiKey = options.apiKey || (process && process.env[Config.ENV_API_KEY]) || "default";
+        this.host = options.host || (process && process.env[Config.ENV_HOST]) || "https://api.us-east-1.mbedcloud.com";
+        this.logLevel = options.logLevel || (process && (process.env[Config.ENV_LOG_LEVEL]) as SDKLogLevel) || "WARN";
         if (this.apiKey.substr(0, 6).toLowerCase() !== "bearer") { this.apiKey = `Bearer ${this.apiKey}`; }
     }
 }
--- test/snippets/foundation/helloworld.ts
+++ test/snippets/foundation/helloworld.ts
@@ -1,5 +1,5 @@
 // tslint:disable:no-console
-import { DeviceRepository, SDK } from "../../../src";
+import { DeviceRepository, SDK, Config } from "../../../src";
 
 describe("hello world examples", () => {
 
@@ -38,6 +38,35 @@ describe("hello world examples", () => {
             // List the first 10 devices in your Pelion DM account
             .list({ maxResults: 10 })
             .executeForAll(device => console.log(`Hello device ${device.name}`));
+        // end of example
+    });
+
+    it("hello world with multiple api keys", () => {
+        const sdk = new SDK();
+        process.env.account_one_api_key = sdk.config.apiKey;
+        process.env.account_two_api_key = sdk.config.apiKey;
+        // an example: hello_world_with_sdk_instance
+         // cloak
+        /*
+        // uncloak
+        import { SDK } from "mbed-cloud-sdk";
+        // cloak
+        */
+        // uncloak
+
+        const accountOne = new SDK({ apiKey: process.env.account_one_api_key });
+        accountOne
+            .foundation()
+            .deviceRepository()
+            .list()
+            .executeForAll(device => console.log(`account one device ${device.name}`));
+
+        const accountTwo = new SDK({ apiKey: process.env.account_two_api_key });
+        accountTwo
+            .foundation()
+            .deviceRepository()
+            .list()
+            .executeForAll(device => console.log(`account two device ${device.name}`));
         // end of example
     });
 
