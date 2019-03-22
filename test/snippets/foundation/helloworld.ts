c80021eea82a5e731b8e5f669f6c0bce9759368b
Alex Logan <Alex.Logan@arm.com>
2019-03-22 14:46:35 +0000

    more hello world example

--- test/snippets/foundation/helloworld.ts
+++ test/snippets/foundation/helloworld.ts
@@ -5,13 +5,39 @@ describe("hello world examples", () => {
 
     it("hello world with device repository", () => {
         // an example: hello_world
-        new DeviceRepository().list().executeForAll(device => console.log(device.name));
+        // cloak
+        /*
+        // uncloak
+        import { DeviceRepository, SDK } from "mbed-cloud-sdk";
+        // cloak
+        */
+        // uncloak
+
+        // create an instance of a device repository
+        new DeviceRepository()
+            // List the first 10 devices in your Pelion DM account
+            .list({ maxResults: 10 })
+            .executeForAll(device => console.log(`Hello device ${device.name}`));
         // end of example
     });
 
     it("hello world with sdk", () => {
         // an example: hello_world_with_sdk_instance
-        new SDK().foundation().deviceRepository().list().executeForAll(device => console.log(device.name));
+        // cloak
+        /*
+        // uncloak
+        import { DeviceRepository, SDK } from "mbed-cloud-sdk";
+        // cloak
+        */
+        // uncloak
+
+        // create an instance of the Pelion Device Management SDK
+        new SDK()
+            .foundation()
+            .deviceRepository()
+            // List the first 10 devices in your Pelion DM account
+            .list({ maxResults: 10 })
+            .executeForAll(device => console.log(`Hello device ${device.name}`));
         // end of example
     });
 
