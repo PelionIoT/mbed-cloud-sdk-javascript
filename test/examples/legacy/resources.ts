6f3ba8d04205a402af20e5468f9966562aec73e1
Alex Logan <Alex.Logan@arm.com>
2019-05-17 13:45:39 +0100

    make legacy examples full examples

--- test/examples/legacy/resources.ts
+++ test/examples/legacy/resources.ts
@@ -4,11 +4,28 @@ import { BillingApi } from "../../../src";
 describe("examples for reading legacy resources", () => {
 
     test("reading a resource", async () => {
+        // an example: legacy_get_resource
+        // cloak
+        /*
+        // uncloak
+        import { BillingApi } from "mbed-cloud-sdk";
+        // cloak
+        */
+        // uncloak
         const billingApi = new BillingApi();
         console.log(`Quota remaining: ${await billingApi.getQuotaRemaining()}`);
+        // end of example
     });
 
     test("listing a resource", () => {
+        // an example: legacy_listing_resources
+        // cloak
+        /*
+        // uncloak
+        import { BillingApi } from "mbed-cloud-sdk";
+        // cloak
+        */
+        // uncloak
         const billingApi = new BillingApi();
         billingApi.getQuotaHistory()
             .then(data => {
@@ -16,6 +33,7 @@ describe("examples for reading legacy resources", () => {
                     console.log(`Quota change reason: ${quotaHistory.reason}, delta: ${quotaHistory.delta}`);
                 });
             });
+        // end of example
     });
 
 });
