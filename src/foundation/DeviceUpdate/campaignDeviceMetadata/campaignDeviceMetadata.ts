b83d2ba4feb6c7189724e9aee0659e4651142015
monty <monty-bot@arm.com>
2019-10-10 10:48:33 +0000

    New Foundation Interface changes

--- src/foundation/DeviceUpdate/campaignDeviceMetadata/campaignDeviceMetadata.ts
+++ src/foundation/DeviceUpdate/campaignDeviceMetadata/campaignDeviceMetadata.ts
@@ -5,53 +5,53 @@ import { CampaignDeviceMetadataDeploymentState } from "./types";
  */
 export interface CampaignDeviceMetadata extends Entity {
     /**
-     *The device's campaign ID
+     *The device's campaign ID.
      *@example 015bf72fccda00000000000100100280
      */
     campaignId: string;
 
     /**
-     *The time the campaign was created
+     *The time the entity was created.
      *@example 2017-05-22T12:37:55.576563Z
      */
     readonly createdAt?: Date;
 
     /**
-     *The state of the update campaign on the device
+     *The state of the update campaign on the device.
      */
     readonly deploymentState?: CampaignDeviceMetadataDeploymentState;
 
     /**
-     *Description
+     *Description.
+     *@example a description
      */
     readonly description?: string;
 
     /**
-     *The device ID
+     *The device ID.
      *@example 015c2fec9bba0000000000010010036f
      */
     readonly deviceId?: string;
 
     /**
-     *How the firmware is delivered (connector or direct)
+     *How the firmware is delivered (connector or direct).
      *@example connector
      */
     readonly mechanism?: string;
 
     /**
-     *The Device Management Connect URL
+     *The Device Management Connect URL.
      */
     readonly mechanismUrl?: string;
 
     /**
-     *The record name
-     *@example default_object_name
+     *The record name.
      */
     readonly name?: string;
 
     /**
-     *The record was modified in the database format: date-time
-     *@example 2017-05-22T12:37:58.776736Z
+     *The time the entity was updated.
+     *@example 2017-05-22T12:37:55.576563Z
      */
     readonly updatedAt?: Date;
 }
