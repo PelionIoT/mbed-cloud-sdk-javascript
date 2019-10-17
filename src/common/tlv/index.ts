3419a5f6ac0ca870b394c3a2964910bc7f5fb14d
Alex Logan <Alex.Logan@arm.com>
2019-10-17 11:37:03 +0100

    move portal tlv parser into sdk

---
+++ src/common/tlv/index.ts
@@ -0,0 +1,5 @@
+import * as Serialization from "./utils/serialization";
+import * as Strings from "./utils/strings";
+
+export { Serialization, Strings };
+export { StringBuilder } from "./utils/stringBuilder";
