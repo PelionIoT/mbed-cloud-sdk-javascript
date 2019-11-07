e0a4f26b38d9321c421ce011a7f318cc78dff54c
Alex Logan <Alex.Logan@arm.com>
2019-11-07 08:57:48 +0000

    export resurce types properly

--- src/legacy/connect/index.ts
+++ src/legacy/connect/index.ts
@@ -1,3 +1,5 @@
 import { ConnectApi } from "./connectApi";
 export { ConnectApi };
+export { ResourceValue } from "./models/resourceValue";
+export { LatLong } from "./types";
 export default ConnectApi;
