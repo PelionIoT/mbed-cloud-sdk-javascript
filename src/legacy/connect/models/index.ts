3b0e8cdc9e8e5752af98a06180aaa77a67c363fe
Alex Logan <Alex.Logan@arm.com>
2019-11-15 16:18:28 +0000

    document resource value

---
+++ src/legacy/connect/models/index.ts
@@ -0,0 +1,5 @@
+export { ConnectedDevice } from "./connectedDevice";
+export { Metric } from "./metric";
+export { Resource } from "./resource";
+export { ResourceValue } from "./resourceValue";
+export { Webhook } from "./webhook";
