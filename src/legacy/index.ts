6e7cf7f6f736795faaa5e4ace4f99272d9c193df
Alex Logan <Alex.Logan@arm.com>
2020-02-24 15:34:36 +0000

    exports

--- src/legacy/index.ts
+++ src/legacy/index.ts
@@ -1,5 +1,4 @@
 export { BillingApi } from "./billing/billingApi";
-export { BootstrapApi } from "./bootstrap/bootstrapApi";
 export { CertificatesApi } from "./certificates/certificatesApi";
 export { ConnectApi, ResourceValue, LatLong } from "./connect";
 export { DeviceDirectoryApi } from "./deviceDirectory/deviceDirectoryApi";
