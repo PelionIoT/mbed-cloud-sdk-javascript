a2e59f17e1e843d5a8e49f46b2d7f7089af2670e
Alex Logan <alex.logan@arm.com>
2020-04-15 13:22:51 +0100

    remove test file!

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { ConnectApi } from "./src";
-
-const connect = new ConnectApi({
-    autostartNotifications: true,
-    apiKey: "ak_1MDE2ZjE4YzA4YjU2OTI1YWFiNDc0ODJkMDAwMDAwMDA017154289e8cee18ba36734b00000000XXjxaXBAZTkvipuYMMs5EGilVQaincgj",
-});
-
-connect.setResourceValue("123", "1/2/3", 123).then(x => console.log(x));
